c7c440d663e24ec312fc175f50275927bd71ba54
mehran <m.najafi.dev@gmail.com>
2022-04-05 15:10:11 -0700

    create application user-access

---
+++ user-access/src/test/java/com/snappay/useraccess/UserAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snappay.useraccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
