5b0fbe9f0374641b8017ed52bd62bb056fc10975
mehran <m.najafi.dev@gmail.com>
2022-04-06 23:35:20 -0700

    add configs mysql in application.properties and add model user

---
+++ user-access/src/main/java/com/snappay/useraccess/user/model/Users.java
@@ -0,0 +1,31 @@
+package com.snappay.useraccess.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "active")
+    private int active;
+
+}
