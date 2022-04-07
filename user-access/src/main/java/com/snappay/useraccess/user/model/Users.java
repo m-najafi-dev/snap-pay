367972261cbeba2be84a6ad0d4090f3de2d8b019
mehran <m.najafi.dev@gmail.com>
2022-04-07 00:29:12 -0700

    add persoperty username and change propertyt name to firstName in class user

--- user-access/src/main/java/com/snappay/useraccess/user/model/Users.java
+++ user-access/src/main/java/com/snappay/useraccess/user/model/Users.java
@@ -17,14 +17,16 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "password")
-    private String password;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
     @Column(name = "active")
     private int active;
 
