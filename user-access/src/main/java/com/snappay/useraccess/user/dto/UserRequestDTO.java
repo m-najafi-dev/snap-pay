39438c7a5cf807b1ad3cb188172a2f6356d7852d
mehran <m.najafi.dev@gmail.com>
2022-04-08 04:17:12 -0700

    create classes UserRequestDTO, UserResponseDTO on package model user.dto and implement class UserRequestDTO of interface ProtocolRequest and implement class UserResponseDTO of interface ProtocolResponse

---
+++ user-access/src/main/java/com/snappay/useraccess/user/dto/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.snappay.useraccess.user.dto;
+
+import com.snappay.useraccess.protocol.ProtocolRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDTO implements ProtocolRequest {
+}
---
+++ user-access/src/main/java/com/snappay/useraccess/user/dto/UserResponseDTO.java
@@ -0,0 +1,12 @@
+package com.snappay.useraccess.user.dto;
+
+import com.snappay.useraccess.protocol.ProtocolResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO implements ProtocolResponse {
+}
