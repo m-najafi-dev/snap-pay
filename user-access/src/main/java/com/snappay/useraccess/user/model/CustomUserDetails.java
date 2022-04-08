1dcf01c040e5a9405df06c06c96f001ab6cb0a93
mehran <m.najafi.dev@gmail.com>
2022-04-08 03:53:49 -0700

    implements methods interface UserDetails and check method isEnable with property getActive(...) User model

--- user-access/src/main/java/com/snappay/useraccess/user/model/CustomUserDetails.java
+++ user-access/src/main/java/com/snappay/useraccess/user/model/CustomUserDetails.java
@@ -1,9 +1,11 @@
 package com.snappay.useraccess.user.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CustomUserDetails extends User implements UserDetails {
 
@@ -13,31 +15,40 @@ public class CustomUserDetails extends User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+        return getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return super.getUserName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return super.getActive() != 0;
     }
 }
